d0d6ce9b06ad2908fe9f778cec98e3e305147290
Eduardo <edu.casagrande@hotmail.com>
2024-01-19 16:46:38 -0300

    Teste de imagem

--- index.js
+++ index.js
@@ -6,22 +6,22 @@ function trocarCSS() {
 
     // Troca entre os estilos ao clicar no botão
     if (estiloAtual === 1) {
-        estilo.href = '/style/style.css';
+        estilo.href = '../style/style.css';
         estiloAtual = 2;
         // trocar imagem claro / escuro
         var img = document.querySelector("#cdg"); 
-        img.setAttribute('src', '../img/cdg.png');
+        img.setAttribute('src', '/img/cdg.png');
 
         var img = document.querySelector("#mode");
-        img.setAttribute('src', '../img/darkmode.png');
+        img.setAttribute('src', '/img/darkmode.png');
     } else {
-        estilo.href = '/style/lightmode.css';
+        estilo.href = '../style/lightmode.css';
         estiloAtual = 1;
         // trocar imagem claro / escuro
         var img = document.querySelector("#cdg");
-        img.setAttribute('src', '../img/cdg-light.png');
+        img.setAttribute('src', '/img/cdg-light.png');
 
         var img = document.querySelector("#mode");
-        img.setAttribute('src', '../img/lightmode.png');
+        img.setAttribute('src', '/img/lightmode.png');
     }
 }
